b2fb95fa0dc9207ad381f612abc758565657cc6d
414983167@qq.com <topq@shootao.com>
2014-09-16 20:08:57 +0800

    update

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class BlogsPost(models.Model):
+	"""docstring for BlogsPost"""
+	 title = models.CharField(max_length= 150)  
+     body = models.TextField()  
+     timestamp = models.DateTimeField()  
