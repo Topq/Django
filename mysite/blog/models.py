a548aaf90fe6eac4e71184536d923ecdd4dbaf17
Topq <414983167@qq.com>
2014-09-19 18:40:58 +0800

    removesite

--- mysite/blog/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class BlogsPost(models.Model):
-	"""docstring for BlogsPost"""
-	 title = models.CharField(max_length= 150)  
-     body = models.TextField()  
-     timestamp = models.DateTimeField()  
